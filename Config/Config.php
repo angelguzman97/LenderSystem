21057966e162df98d7f3536fc1bcecacee2b77e7
angelguzman97 <109249659+angelguzman97@users.noreply.github.com>
2023-05-11 00:59:58 -0600

    Create Config.php

---
+++ Config/Config.php
@@ -0,0 +1,8 @@
+<?php
+const base_url = "http://localhost/brenda/";
+const host = "localhost";
+const user = "root";
+const pass = "";
+const db = "sistemas_prestamos";
+const charset = "charset=utf8";
+?>
